a08cc8b04827812c3739b2e2e67c2c715a7faf1a
Arseny Ptitsyn <senyabird@yandex.ru>
2023-02-01 19:26:45 +0300

    Game modes added!

--- homework9/candies.py
+++ homework9/candies.py
@@ -3,15 +3,28 @@ from random import randint
 candies = 221
 player1 = None
 player2 = None
+game_mode = 1
 
 
-def players_representation():
+def choose_game_mode():
+    global game_mode
+    game_mode = int(input('Если вы хотите играть с человеком, нажмите "1", если с компьютером - "2".'))
+    return game_mode
+
+
+def pvp_representation():
     global player1, player2
     player1 = input('Первый игрок, введите своё имя: ')
     player2 = input('Второй игрок, введите своё имя: ')
 
 
-def turn(player):
+def pvm_representation():
+    global player1, player2
+    player1 = input('Введите своё имя: ')
+    player2 = 'Компьютер'
+
+
+def human_turn(player):
     global candies
     while True:
         print(f'Сейчас на столе {candies} конфет!')
@@ -27,6 +40,13 @@ def turn(player):
             break
 
 
+def ai_turn():
+    global candies
+    taken_candies = candies % 29
+    candies -= taken_candies
+    print(f'Компьютер взял {taken_candies} конфет и теперь на столе {candies} конфет!')
+
+
 def check_win(player):
     global candies
     return candies == 0
@@ -38,6 +58,7 @@ def toss():
     else:
         return player2
 
+
 def change_player(player):
     global player1, player2
     if player == player1:
@@ -46,11 +67,30 @@ def change_player(player):
         return player1
 
 
-players_representation()
-player = toss()
-while True:
-    turn(player)
-    if check_win(player):
-        print(f'{player} win!')
-        break
-    player = change_player(player)
+game_mode = choose_game_mode()
+if game_mode == 1:
+    pvp_representation()
+    player = toss()
+    while True:
+        human_turn(player)
+        if check_win(player):
+            print(f'{player} win!')
+            break
+        player = change_player(player)
+elif game_mode == 2:
+    pvm_representation()
+    player = toss()
+    while True:
+        if player == 'AI':
+            ai_turn()
+            if check_win(player):
+                print(f'{player} win!')
+                break
+            player = change_player(player)
+        else:
+            human_turn(player)
+            if check_win(player):
+                print(f'{player} win!')
+                break
+            player = change_player(player)
+
